1c029cb52cac590cb97f38e4a6bf560b3b6ec18b
Rudresh <rudreshsankpal21@gmail.com>
2026-05-15 21:34:38 +0530

    generate random number

--- index.php
+++ index.php
@@ -1,21 +1,7 @@
-<!--  Generate a basic website with css -->
+<!-- Generate a random number -->
+<?php
+$randomNumber = rand(1,99);
+?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <style>
-        h1{
-            color: red;
-        }
-    </style>
-</head>
-<body>
-    <h1>Hello World</h1>
-    <?php
-    echo "<h1>Hello World</h1>";
-    ?>
-</body>
-</html>
+<!-- Display the random number -->
+<p>The random number is: <?php echo $randomNumber; ?></p>
