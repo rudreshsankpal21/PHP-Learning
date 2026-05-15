1c6989ecc52186e863f777842ce330eeddea9e38
Rudresh <rudreshsankpal21@gmail.com>
2026-05-15 21:49:12 +0530

    practice marks & percentage calculator

---
+++ marks.php
@@ -0,0 +1,37 @@
+<?php 
+$english = 98;
+$maths = 90;
+$science = 95;
+
+$total = $english + $maths + $science;
+$percentage = ($total / 300) * 100;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Marks Percentage Calculator</h1>
+
+    <h2>Total Marks: <?php echo $total; ?></h2>
+    <h2>Percentage: <?php echo $percentage; ?></h2>
+    <h2>
+
+     <?php
+     
+        if ($percentage >= 35) {
+            echo "<h2>Pass</h2>";
+        } else {
+            echo "<h2>Fail</h2>";
+        }
+
+
+     ?>
+
+     </h2>
+</body>
+</html>
